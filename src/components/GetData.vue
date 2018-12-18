8318a9150d924d8eece83ffb2661fa03dc23ff0e
Kurniandha S.Y <ykurniandha@gmail.com>
2018-12-19 01:30:44 +0700

    Initial commit

---
+++ src/components/GetData.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="GetData">
+    <button v-on:click="getData">GET</button>
+    <div v-if="msg != null">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'GetData',
+  data () {
+    return {
+      msg: null
+    }
+  },
+  methods: {
+    getData: function (event) {
+      axios
+        .get('https://api.github.com/zen')
+        .then(response => (this.msg = response.data))
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VueRouter from 'vue-router'
+import GetData from './components/GetData'
+import About from './components/About'
+
+Vue.use(VueRouter)
+const routes = [
+  { path: '/', component: GetData },
+  { path: '/about', component: About }
+]
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
+  router
+}).$mount('#app')
